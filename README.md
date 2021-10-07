413a856fb90d5280365481ddac1d9af867aa3521
DilkiC <dilkicooray97@gmail.com>
2021-10-07 22:14:33 +0530

    'intialized'

---
+++ README.md
@@ -0,0 +1,10 @@
+this project is xxx
+
+app run
+    setup gradle and run
+
+app build
+    gradle build
+
+deployment instructions
+    build and get jar then deploy
---
+++ src/A.java
@@ -0,0 +1,14 @@
+/*
+ * ................................................................
+ *  Copyright (c) GDSE-55.year. All rights received.
+ *  License under Sri Lankan informations.
+ * .......................................................................
+ *
+ */
+
+/**
+ * @author Dilki Cooray <dilkicooray97@gmail.com>
+ * since 10/7/2021
+ */
+public class A {
+}
